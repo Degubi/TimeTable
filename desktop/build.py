7db7dc52746f17c057f2d421c16ab84fefa97570
Degubi <gergonoorbi@gmail.com>
2020-09-23 17:33:00 +0200

    Update backend dependencies

--- desktop/build.py
+++ desktop/build.py
@@ -1,4 +1,4 @@
-from subprocess import call
+from subprocess import call, DEVNULL
 from shutil import rmtree
 from os import rename
 
@@ -13,20 +13,20 @@ print('Generating runtime')
 call(jlinkCommand)
 
 print('Copying libraries')
-call('mvn dependency:copy-dependencies -DincludeScope=runtime -DoutputDirectory=lib', shell = True)
+call('mvn dependency:copy-dependencies -DincludeScope=runtime -DoutputDirectory=resources', shell = True, stdout = DEVNULL)
 
 print('Creating TimeTable.jar')
-call('mvn package', shell = True)
-rename('target/TimeTable_Desktop-1.0.jar', 'lib/TimeTable.jar')
+call('mvn package -Dmaven.test.skip=true', shell = True, stdout = DEVNULL)
+rename('target/TimeTable_Desktop-1.0.jar', 'resources/TimeTable.jar')
 
 print('Creating installer file')
-call((r'jpackage --runtime-image runtime -i lib --main-class timetable.Main --main-jar TimeTable.jar '
+call((r'jpackage --runtime-image runtime -i resources --main-class timetable.Main --main-jar TimeTable.jar '
       r'--name TimeTable --vendor Degubi --description TimeTable --icon icon.ico '
       r'--win-per-user-install --win-dir-chooser --win-shortcut'))
 
 print('Cleaning up')
 rename('TimeTable-1.0.exe', 'TimeTableInstaller.exe')
-rmtree('lib')
+rmtree('resources')
 rmtree('runtime')
 
 print('\nDone!')
